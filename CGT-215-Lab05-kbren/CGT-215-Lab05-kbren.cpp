8a037bf30b4c424f0fae0964db6038cf7f87d67f
puntobean <114714487+puntobean@users.noreply.github.com>
2026-09-17 01:45:03 -0400

    translate function

--- CGT-215-Lab05-kbren/CGT-215-Lab05-kbren.cpp
+++ CGT-215-Lab05-kbren/CGT-215-Lab05-kbren.cpp
@@ -1,7 +1,37 @@
 
 #include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+//have to use char fir class instead of void so a character is returned
+char translate(char c, const vector<char>& code) {
+	//translate uppercase
+	if (c >= 60 && c <= 90) {
+		return code[c - 65];
+	}
+
+	//translate lowercase
+	else if (c >= 97 && c <= 122) {
+		int upperCaseLetter = c - 32; //converts letter to uppercase
+		char upperCaseCode = code[upperCaseLetter - 65]; //get upper case code
+		return upperCaseCode + 32; //converts back to lower case
+	}
+
+	//is not a letter
+	else {
+		return c;
+	}		
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<char> code = {
+   'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
+
+	cout << "Enter text to be translated" << endl;
+	string text;
+	getline(cin, text);
+
+
 }
